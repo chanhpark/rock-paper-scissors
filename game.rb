825908f172a8dd32ff76925229c80330d5b42ed4
chanhpark <chanhyun.park1211@gmail.com>
2014-11-03 22:03:02 -0500

    small change of indentation

--- game.rb
+++ game.rb
@@ -1,70 +1,70 @@
-c_points=0
-p_points=0
+c_points = 0
+p_points = 0
 
 while p_points !=2 && c_points !=2
   puts "Player: #{p_points}, Computer: #{c_points}"
-  i=rand(3)
-  if i==0
-    comp='r'
-    c_input='rock'
-  elsif i==1
-    comp='s'
-    c_input='scissors'
-  elsif i==2
-    comp='p'
-    c_input='paper'
+  i = rand(3)
+  if i == 0
+    comp = 'r'
+    c_input = 'rock'
+  elsif i == 1
+    comp = 's'
+    c_input = 'scissors'
+  elsif i == 2
+    comp = 'p'
+    c_input = 'paper'
   end
 
-  puts "\nLets play Rock, Paper, Scissors\nSelect r (Rock), p (Paper) or s (Scissors)"
-  player= gets.chomp
+    puts "\nLets play Rock, Paper, Scissors\nSelect r (Rock), p (Paper) or s (Scissors)"
+    player = gets.chomp
 
-  if player=='r'
-     p_input='rock'
+  if player == 'r'
+     p_input = 'rock'
 
-    if comp=='r'
-       win='Draw'
-    elsif comp=='s'
-          win='Player'
-    else comp=='p'
-         win='Computer'
+    if comp == 'r'
+       win = 'Draw'
+    elsif comp == 's'
+          win = 'Player'
+    else comp == 'p'
+         win = 'Computer'
     end
 
-  elsif player=='s'
-        p_input='scissors'
+  elsif player == 's'
+        p_input = 'scissors'
 
-    if comp=='r'
-       win='Computer'
-    elsif comp=='s'
-          win='Draw'
-    else comp=='p'
-         win='Player'
+    if comp == 'r'
+       win = 'Computer'
+    elsif comp == 's'
+          win = 'Draw'
+    else comp == 'p'
+         win = 'Player'
     end
 
-  elsif player=='p'
-     p_input='paper'
+  elsif player == 'p'
+     p_input = 'paper'
 
-     if comp=='r'
-        win='Player'
-     elsif comp=='s'
-           win='Computer'
-     else comp=='p'
-          win='Draw'
+     if comp == 'r'
+        win = 'Player'
+     elsif comp == 's'
+           win = 'Computer'
+     else comp == 'p'
+          win = 'Draw'
       end
 
   else
     puts "You\'re freaking me out!\nI can only read r(Rock), p(Paper) or s(Scissors)"
   end
 
-  puts "The computer chose: #{c_input} You chose: #{p_input}"
+    puts "The computer chose: #{c_input} You chose: #{p_input}"
 
-  if win=='Player'
+  if win == 'Player'
     puts "Player beats Computer round won by Player"
-    p_points=p_points+1
-  elsif win=='Computer'
+    p_points = p_points + 1
+  elsif win == 'Computer'
     puts "Computer beats Player round won by Computer"
-    c_points=c_points+1
+    c_points = c_points + 1
     puts "Score: You #{p_points}; Computer #{c_points}"
-  elsif win=='Draw'
+  elsif win == 'Draw'
     "Draw! please choose again."
      puts "Score: You #{p_points}; Computer #{c_points}"
   end
